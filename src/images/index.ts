52f10c7ec5e7c9408abb69f8c1e2e4eeb0951152
wedding260314 <jainy0v0@gmail.com>
2026-01-20 23:16:41 +0900

    Update index.ts

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,33 +1,45 @@
 import coverImage from "./cover1.jpg"
-import photo1 from "./photo1.jpg"
-import image2 from "./image2.png"
-import image3 from "./image3.png"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import photo1 from "./photo1.JPG"
+import photo2 from "./photo2.JPG"
+import photo3 from "./photo3.JPG"
+import photo4 from "./photo4.JPG"
+import photo5 from "./photo5.JPG"
+import photo6 from "./photo6.JPG"
+import photo7 from "./photo7.JPG"
+import photo8 from "./photo8.JPG"
+import photo9 from "./photo9.JPG"
+import photo10 from "./photo10.JPG"
+import photo11 from "./photo11.JPG"
+import photo12 from "./photo12.JPG"
+import photo13 from "./photo13.JPG"
+import photo14 from "./photo14.JPG"
+import photo15 from "./photo15.JPG"
+import photo16 from "./photo16.JPG"
+import photo17 from "./photo17.JPG"
+import photo18 from "./photo18.JPG"
 import ending32 from "./ending3-2.JPG"
 
 export const COVER_IMAGE = coverImage
 
 export const GALLERY_IMAGES = [
   photo1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
-  image10,
-  image11,
-  image12,
+  photo2,
+  photo3,
+  photo4,
+  photo5,
+  photo6,
+  photo7,
+  photo8,
+  photo9,
+  photo10,
+  photo11,
+  photo12,
+  photo13,
+  photo14,
+  photo15,
+  photo16,
+  photo17,
+  photo18,
 ]
 
 export const ENDING_IMAGE = {
